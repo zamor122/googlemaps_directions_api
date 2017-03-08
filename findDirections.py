0e153cf2c8bfc1bee0e4ede2121b288bb860b2ab
Shayne Zamora <zamor122@mail.chapman.edu>
2017-03-07 21:39:36 -0800

    Updates Comments and adds new functionality

--- findDirections.py
+++ findDirections.py
@@ -1,13 +1,7 @@
-'''
-This is a sample program in python that gives turn by turn directions in a readable format.
-In order to code this program please follow the steps below:
-1. Go to https://developers.google.com/api-client-library/python/
-    This explains what the API is and how it can be used.
-2. Click "Get Started" and It will bring you to this link: https://developers.google.com/api-client-library/python/start/get_started
-    Here, follow the steps to sign up or login to your Google account and obtain an API key and install the googlemaps library.
-    Please follow all the steps as you need to have the Googlemaps library installed on your computer in order to run this program.
-3.
-'''
+######################################################################
+#Program that prints directions from a starting point to a destination until user quits
+# Author: @Shayne_Zamora
+######################################################################
 import googlemaps
 import json, urllib
 from urllib import urlencode
@@ -15,15 +9,42 @@ import re
 import os.path
 #authorizing client using key
 gmaps = googlemaps.Client(key='AIzaSyB_VMjaJzWr9U6RatHtmJBoSrEbOWgC7vg')
-start = raw_input('Enter the address you are departing from: ')
-finish = raw_input('Enter the address of the desination: ')
-url = 'http://maps.googleapis.com/maps/api/directions/json?%s' % urlencode((
-    ('origin', start),
-    ('destination', finish)
-))
-ur = urllib.urlopen(url)
-result = json.load(ur)
-for i in range (0,len (result['routes'][0]['legs'][0]['steps'])):
-    uglyDirections = result['routes'][0]['legs'][0]['steps'][i]['html_instructions']
-    niceDirections = re.sub('<[^>]*>','',uglyDirections)
-    print niceDirections
+def sendRequest():
+    #Get starting point
+    start = raw_input('Enter the address you are departing from: ')
+    #Get ending destination point
+    finish = raw_input('Enter the address of the desination: ')
+    #request from the google url using start and destination points
+    request = 'http://maps.googleapis.com/maps/api/directions/json?%s' % urlencode((
+        ('origin', start),
+        ('destination', finish)
+    ))
+    #store request results
+    requestResults = urllib.urlopen(request)
+    #results in json form
+    result = json.load(requestResults)
+    #sort json form and print out directions nicely so they are readable
+    for i in range (0,len (result['routes'][0]['legs'][0]['steps'])):
+        uglyDirections = result['routes'][0]['legs'][0]['steps'][i]['html_instructions']
+        niceDirections = re.sub('<[^>]*>','',uglyDirections)
+        print '\n'+ niceDirections
+#main function
+def main():
+    running = True
+    directionNumber = 1
+    while running:
+        #get directions
+        sendRequest()
+        #ask user if they would like to get more directions
+        answer = raw_input('Would you like to another set of directions? Y or N')
+        if answer.upper() == 'N':
+            #process exiting
+            print 'Process exited Successfully'
+            running = False
+            break
+        else:
+            #continue with new directions
+            directionNumber+=1
+            print 'Please follow the directions below. This is your' , directionNumber , 'time getting directions.'
+#run main
+main()
